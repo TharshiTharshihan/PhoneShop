c153034b48d42971d0446201c47627d0cb042ecd
tharshihan2000@gmail.com <tharshihan_r_e23@engug.ruh.ac.lk>
2025-04-25 15:38:50 +0530

    admin page modifications

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,16 +6,16 @@ import {
   Navigate,
 } from "react-router-dom";
 import "./App.css";
-import FrontPage from "./pages/FrontPage/frontpage";
-import Signup from "./pages/Register/Signup";
-import Login from "./pages/Register/Login";
+// import FrontPage from "./pages/FrontPage/frontpage";
+// import Signup from "./pages/Register/Signup";
+// import Login from "./pages/Register/Login";
 import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 import CreateProducts from "./pages/AdminDashboard/CreateProducts";
 import UpdateProducts from "./pages/AdminDashboard/UpdateProducts ";
-import CustomerDashboard from "./pages/CustomerDashboard/CustomerDashboard";
-import CartPage from "./pages/Cart/Cart";
-import Success from "./pages/Cart/Success";
-import Cancel from "./pages/Cart/Cancel";
+// import CustomerDashboard from "./pages/CustomerDashboard/CustomerDashboard";
+// import CartPage from "./pages/Cart/Cart";
+// import Success from "./pages/Cart/Success";
+// import Cancel from "./pages/Cart/Cancel";
 
 function App() {
   const ProtectedRoute = () => {
@@ -27,18 +27,18 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<FrontPage />}></Route>
+          {/* <Route path="/" element={<FrontPage />}></Route>
           <Route path="/signup" element={<Signup />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/success" element={<Success />} />
-          <Route path="/cancel" element={<Cancel />} />
+          <Route path="/cancel" element={<Cancel />} /> */}
 
           <Route element={<ProtectedRoute />}>
-            <Route path="/admin" element={<AdminDashboard />}></Route>
+            <Route path="/" element={<AdminDashboard />}></Route>
             <Route path="/create" element={<CreateProducts />}></Route>
             <Route path="/update/:id" element={<UpdateProducts />}></Route>
-            <Route path="/customer" element={<CustomerDashboard />}></Route>
-            <Route path="/cart" element={<CartPage />}></Route>
+            {/* <Route path="/customer" element={<CustomerDashboard />}></Route> */}
+            {/* <Route path="/cart" element={<CartPage />}></Route> */}
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,9 +16,9 @@ import {
 import FrontPage from "./pages/FrontPage/frontpage";
 import Signup from "./pages/Register/Signup";
 import Login from "./pages/Register/Login";
-import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
-import CreateProducts from "./pages/AdminDashboard/CreateProducts";
-import UpdateProducts from "./pages/AdminDashboard/UpdateProducts ";
+// import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
+// import CreateProducts from "./pages/AdminDashboard/CreateProducts";
+// import UpdateProducts from "./pages/AdminDashboard/UpdateProducts ";
 import CustomerDashboard from "./pages/CustomerDashboard/CustomerDashboard";
 import CartPage from "./pages/Cart/Cart";
 import Success from "./pages/Cart/Success";
@@ -41,9 +41,9 @@ function App() {
           <Route path="/cancel" element={<Cancel />} />
 
           <Route element={<ProtectedRoute />}>
-            <Route path="/admin" element={<AdminDashboard />}></Route>
+            {/* <Route path="/admin" element={<AdminDashboard />}></Route>
             <Route path="/create" element={<CreateProducts />}></Route>
-            <Route path="/update/:id" element={<UpdateProducts />}></Route>
+            <Route path="/update/:id" element={<UpdateProducts />}></Route> */}
             <Route path="/customer" element={<CustomerDashboard />}></Route>
             <Route path="/cart" element={<CartPage />}></Route>
           </Route>
